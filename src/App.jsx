63ab0070954f411b4dd4be8429d74bda22766158
Yaswanth babu Patnam <yaswanthpatnam1819@gmail.com>
2025-09-04 10:44:49 +0530

    Updated CRUD operations using API

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoForm } from "./components/TodoForm/TodoForm";
 import { TodoList } from "./components/TodoList/TodoList";
 import styles from "./App.module.css";
-
-const TODOS_DEFAULT = [
-  {
-    id: "1",
-    name: "Buy an Ice Cream",
-    description: "The white one with chocolate",
-    deadline: "2025-02-09",
-    priority: "low",
-    completed: false,
-  },
-  {
-    id: "2",
-    name: "Sell old MacBook Pro 2025",
-    description: "Try to sell it on OLX",
-    deadline: "2025-02-28",
-    priority: "high",
-    completed: false,
-  },
-  {
-    id: "3",
-    name: "Charge Powerbank",
-    description: "For the next travelling",
-    deadline: "2025-02-15",
-    priority: "medium",
-    completed: true,
-  },
-  {
-    id: "4",
-    name: "Test Todo onlye with a name",
-    description: "",
-    deadline: "",
-    priority: "none",
-    completed: false,
-  },
-];
+import { TodoFilters } from "./components/TodoFilters/TodoFilters";
 
 function App() {
-  const [todos, setTodos] = useState(TODOS_DEFAULT);
+  const [todos, setTodos] = useState([]);
+  const [filters, setFilters] = useState({});
+
+  function fetchTodos() {
+    const searchParams = new URLSearchParams(filters).toString();
+    fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos?${searchParams}`, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((response) => {
+        if (response.ok) return response.json();
+        if (response.status === 404) return [];
+      })
+      .then(setTodos);
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, [filters]);
 
   function handleCreate(newTodo) {
-    setTodos((prevTodos) => [
-      ...prevTodos,
-      { id: `${prevTodos.length + 1}`, ...newTodo },
-    ]);
+    fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(newTodo),
+    })
+      .then((response) => !!response.ok && response.json())
+      .then(fetchTodos);
   }
 
   function handleUpdate(id, newTodo) {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) => (todo.id === id ? newTodo : todo))
-    );
+    fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos/${id}`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(newTodo),
+    })
+      .then((response) => !!response.ok && response.json())
+      .then(fetchTodos);
+  }
+
+  function handleDelete(id) {
+    fetch(`${import.meta.env.VITE_MOCKAPI_BASE_URL}todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => !!response.ok && response.json())
+      .then(fetchTodos);
   }
 
   return (
@@ -63,7 +62,12 @@ function App() {
 
       <div className={styles.AppContainer}>
         <TodoForm onCreate={handleCreate} />
-        <TodoList todos={todos} onUpdate={handleUpdate} />
+        <TodoFilters onFilter={setFilters} />
+        <TodoList
+          todos={todos}
+          onUpdate={handleUpdate}
+          onDelete={handleDelete}
+        />
       </div>
     </div>
   );
